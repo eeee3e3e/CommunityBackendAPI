676d76b1c182708ad976ed66be4a8a3182e7a320
Yang <yang@rayootech.com>
2025-01-12 23:57:22 +0800

    update port in Program.cs

--- CommunityBackendAPI/Program.cs
+++ CommunityBackendAPI/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
     new MySqlServerVersion(new Version(8, 0, 40))));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -33,4 +34,5 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+// Bind to all IP addresses (0.0.0.0) and port 8080
+app.Run("http://0.0.0.0:8080");  // 绑定到0.0.0.0, 使外部请求可访问
