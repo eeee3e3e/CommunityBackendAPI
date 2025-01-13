da5132bcb455a41c3da01a153abfd20e93689732
Yang <yang@rayootech.com>
2025-01-14 01:25:55 +0800

    6

--- CommunityBackendAPI/Program.cs
+++ CommunityBackendAPI/Program.cs
@@ -1,39 +1,29 @@
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.Identity.Web;
 using CommunityBackendAPI.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+// 移除身份验证配置
+// builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//     .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
     new MySqlServerVersion(new Version(8, 0, 40))));
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
- 
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseDeveloperExceptionPage();  // 仅在开发环境中启用详细错误信息
 app.UseHttpsRedirection();  // 如果要启用 HTTPS，请保留此行
 
-app.UseAuthorization();
+app.UseAuthorization();  // 如果你希望在全局范围内启用授权，可以保留，但可以暂时移除这行来测试匿名访问
 
 app.MapControllers();
- 
-
-app.Run();  // 绑定到0.0.0.0, 使外部请求可访问
+app.Run();
