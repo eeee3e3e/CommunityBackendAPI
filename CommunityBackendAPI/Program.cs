8c6cfbe84f8badf3a7f53646c0493f65a179b5e2
Yang <yang@rayootech.com>
2025-01-14 01:37:59 +0800

    1

--- CommunityBackendAPI/Program.cs
+++ CommunityBackendAPI/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.WebHost.UseUrls("http://*:8080");
 var app = builder.Build();
 
 app.UseSwagger();
