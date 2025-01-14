ed23827f84185efddecd1757e4debdf60bb76e43
Yang <yang@rayootech.com>
2025-01-14 16:38:04 +0800

    your commit message

--- CommunityBackendAPI/Program.cs
+++ CommunityBackendAPI/Program.cs
@@ -1,59 +1,33 @@
 using CommunityBackendAPI.Data;
 using Microsoft.EntityFrameworkCore;
 
-//var builder = WebApplication.CreateBuilder(args);
-
-//// 移除身份验证配置
-//// builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-////     .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
-
-//builder.Services.AddControllers();
-//builder.Services.AddDbContext<AppDbContext>(options =>
-//    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
-//    new MySqlServerVersion(new Version(8, 0, 40))));
-
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-//builder.WebHost.UseUrls("http://0.0.0.0:8080");
-//var app = builder.Build();
-
-//app.UseSwagger();
-//app.UseSwaggerUI(options =>
-//{
-//    options.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
-//});
-
-
-//app.UseDeveloperExceptionPage();  // 仅在开发环境中启用详细错误信息
-//app.UseHttpsRedirection();  // 如果要启用 HTTPS，请保留此行
-
-//app.UseAuthorization();  // 如果你希望在全局范围内启用授权，可以保留，但可以暂时移除这行来测试匿名访问
-
-//app.MapControllers();
-//app.Run();
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// 移除身份验证配置
+// builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//     .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+    new MySqlServerVersion(new Version(8, 0, 40))));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-// 使用 WebHost 配置监听所有 IP 地址
 builder.WebHost.UseUrls("http://0.0.0.0:8080");
 var app = builder.Build();
 
-// Configure the HTTP request pipeline. 
 app.UseSwagger();
 app.UseSwaggerUI(options =>
 {
     options.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
 });
 
-app.UseAuthorization();
 
+app.UseDeveloperExceptionPage();  // 仅在开发环境中启用详细错误信息
+app.UseHttpsRedirection();  // 如果要启用 HTTPS，请保留此行
+
+app.UseAuthorization();  // 如果你希望在全局范围内启用授权，可以保留，但可以暂时移除这行来测试匿名访问
 
 app.MapControllers();
-
 app.Run();
